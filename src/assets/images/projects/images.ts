5a52712a7d348fcc935c4b4e27cc6cfd5c2cab40
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-05 23:07:29 +0200

    Add export file form project images

---
+++ src/assets/images/projects/images.ts
@@ -0,0 +1,17 @@
+import portfolioImgUrl from './portfolio-img.png';
+import portfolioGifUrl from './portfolio-gif.gif';
+import shopImgUrl from './shop-img.png';
+import shopGifUrl from './shop-gif.gif';
+import keylearnImgUrl from './keylearn-img.png';
+import keylearnGifUrl from './keylearn-gif.gif';
+
+const images: Record<string, string> = {
+  portfolioImgUrl,
+  portfolioGifUrl,
+  shopImgUrl,
+  shopGifUrl,
+  keylearnImgUrl,
+  keylearnGifUrl,
+};
+
+export default images;
