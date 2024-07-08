0080a6e1d85cb4ca791aeb0a2ca87704f29be1f2
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-08 23:55:42 +0200

    Add Layout/FlexRow component

---
+++ src/layouts/flex_row/FlexRow.tsx
@@ -0,0 +1,14 @@
+import { ElementType, ReactNode } from 'react';
+
+type FlexRowProps = {
+  Tag: ElementType;
+  className?: string;
+  children: ReactNode;
+};
+
+const FlexRow = ({ Tag, className, children }: FlexRowProps) => {
+  const additionalClass = className ? `${className} ` : '';
+  return <Tag className={additionalClass + 'flex_row'}>{children}</Tag>;
+};
+
+export default FlexRow;
